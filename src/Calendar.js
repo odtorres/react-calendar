eec1bdb68fdf1ec945f51cd8389cb9067fa3fea6
Oscar Torres <odtorres891118@gmail.com>
2017-03-23 09:24:59 -0400

    Calendar updated

--- src/Calendar.js
+++ src/Calendar.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './Calendar.css';
 import UtilDate from './UtilDate';
-//import EventHandler from './EventHandler';
+import EventHandler from './EventHandler';
 import EventList from './EventList';
 
 class Calendar extends Component {
@@ -17,7 +17,10 @@ class Calendar extends Component {
     this.monthBefore = this.monthBefore.bind(this);
     this.monthAfter = this.monthAfter.bind(this);
     this.setCurrentMonthDate = this.setCurrentMonthDate.bind(this);
-    this.eventClickHandle = this.eventClickHandle.bind(this);
+    this.showEventList = this.showEventList.bind(this);
+    this.showCalendar = this.showCalendar.bind(this);
+    this.addEvent = this.addEvent.bind(this);
+    this.removeEvent = this.removeEvent.bind(this);
   }
 
   setCurrentMonthDate(plus) {
@@ -40,22 +43,45 @@ class Calendar extends Component {
     this.setCurrentMonthDate(-1);
   }
 
-  eventClickHandle(e) {
-    console.debug(this.state.currentYear);
-    console.debug(this.state.currentMonth + 1);
-    console.debug(Object.values(e.target)[0]._hostNode.innerText);
+  showEventList(e) {
+    this.clickDate = this.state.currentYear + "-" +
+      this.state.currentMonth + "-" +
+      Object.values(e.target)[0]._hostNode.innerText;
     this.setState({
-      visible:false
+      visible: false
     });
+  }
+
+  showCalendar() {
+    this.setState({
+      visible: true
+    });
+  }
+
+  addEvent(name, details, from, to) {
+    if (!EventHandler.events[this.clickDate]) {
+      EventHandler.events[this.clickDate] = {};
+      EventHandler.events[this.clickDate].dayEvents = [];
+    }
+    EventHandler.events[this.clickDate].dayEvents.push({
+      name: name,
+      details: details,
+      from: from,
+      to: to
+    });
+  }
+
+  removeEvent(i) {
+    EventHandler.events[this.clickDate].dayEvents.splice(i, 1);
   }
 
   render() {
     let days = UtilDate.daysInMonth(this.state.currentYear, this.state.currentMonth);
     let firstDay = UtilDate.getFirstDay(this.state.currentYear, this.state.currentMonth);
     let day = 0;
-    if (this.state.visible){ 
+    if (this.state.visible) {
       return (
-        <div className="calendar"> 
+        <div className="calendar">
           <div className="year">
             <div >{this.state.currentYear}</div>
           </div>
@@ -78,15 +104,15 @@ class Calendar extends Component {
                   return <div key={i + " "}> {"  "} </div>
                 if (days <= day)
                   return <div key={i + " "}> {"  "} </div>
-                return <div key={++day} className="validDays" onClick={this.eventClickHandle}> {day} </div>
+                return <div key={++day} className="validDays" onClick={this.showEventList}> {day} </div>
               })
             }
-          </div>          
+          </div>
         </div>
       );
-    }else{
+    } else {
       return (
-        <EventList></EventList>
+        <EventList done={this.showCalendar} addEvent={this.addEvent} events={this.clickDate} removeEvent={this.removeEvent}></EventList>
       );
     }
   }
